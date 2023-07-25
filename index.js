156198f0e10817515293acd8c2e82470164a8024
Jesse Reynolds <jessereynolds18@gmail.com>
2023-07-25 04:27:30 -0500

    Makes several functions to either delete or update keys in the employees object or a cloned object named newEmployees

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+const employee = {
+    name: "Unnamed",
+    streetAddress: "No Address",
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newEmployee = {...employee}
+    newEmployee.name = "Sam"
+    newEmployee.streetAddress = "11 Broadway";
+    return newEmployee;
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    employee.name = "Sam";
+    employee.streetAddress = "12 Broadway";
+    return employee;
+};
+
+function deleteFromEmployeeByKey(obj, key) {
+    const newEmployee = {...employee};
+    delete newEmployee.name;
+    return newEmployee;
+};
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete employee.name;
+    return employee;
+};
